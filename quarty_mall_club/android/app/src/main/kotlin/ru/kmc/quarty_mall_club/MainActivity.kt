5d200a7d8ba1cf2ad3ca8ecb98c9b104d2afb21a
Letch <guediagael@gmail.com>
2019-03-07 01:23:16 +0300

    Initial commit

---
+++ quarty_mall_club/android/app/src/main/kotlin/ru/kmc/quarty_mall_club/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.kmc.quarty_mall_club
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
